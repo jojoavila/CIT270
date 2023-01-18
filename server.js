f4b25b65bccba44a6fd35d29312c99ff605dc117
jojoavila <avilajosuealejandro@gmail.com>
2023-01-18 13:14:07 -0700

    writing the API initial lines of code

--- server.js
+++ server.js
@@ -1,15 +1,25 @@
-const express = require("express");
+const express = require('express');
 
 const app = express();
 
 const port = 3000;
 
-app.get("/", (req, res) => {
-    res.send("Hello Josue");
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.json()); // look for incoming data
+
+app.get('/', (req, res) => {
+    res.send('Hello Josue');
+});
+
+app.post('/login', (req, res) => {
+    const loginUser = req.body.userName;
+    console.log('Login Username: '+loginUser);
+    res.send('Hello '+loginUser);
 });
 
 app.listen(port, () => {
-    console.log("listening");
+    console.log('listening');
 });
 
 // localhost:3000
