fbd34d12b543342b0edfbb6fb312bc1c7b41dd71
jojoavila <avilajosuealejandro@gmail.com>
2023-01-25 13:41:13 -0700

    making the redis server

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const bodyParser = require('body-parser');
 
 const Redis = require('redis');
 
-const redisClient = Redis.createClient();
+const redisClient = Redis.createClient({url:'redis://127.0.0.1:6379'});
 
 const {v4: uuidv4} = require('uuid')
 
