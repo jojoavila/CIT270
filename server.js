79676b432f14c956aee09ef65785906df43def20
jojoavila <avilajosuealejandro@gmail.com>
2023-02-06 13:45:51 -0700

    saving data into server; and checking the cookie (token) constantly

--- server.js
+++ server.js
@@ -20,9 +20,22 @@ const cookieParser = require("cookie-parser");
 
 app.use(cookieParser());
 
+app.use(async function (req, res, next){
+    var cookie = req.cookies.stedicookie;
+    if(cookie === undefined && !req.url.includes("login") && !req.url.includes("html") && req.url !== '/' && req.url.includes('css') && !req.url.includes('js') && !req.url.includes('ico') && !req.url.includes('png')) {
+        // no: set a new cookie
+        res.status(401);
+        res.send('no cookie');
+    }
+    else{
+        // yes: coockie was already present
+        res.status(200);
+        next();
+    } 
+});
+
 app.post('/rapidsteptest', async(req, res) => {
     const steps = req.body;
-    // await redisClient.zAdd('Steps', steps, 0);
     await redisClient.zAdd('Steps',[{score:0,value:JSON.stringify(steps)}]);
     console.log('Steps',steps);
     res.send('saved');
